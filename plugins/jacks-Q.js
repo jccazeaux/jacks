1b8cd3fa896b660a24a234847a910bfe53d69176
Jean-Christophe Cazeaux <jccazeaux1979@gmail.com>
2015-04-19 17:45:18 +0200

    New plugin definition

--- plugins/jacks-Q.js
+++ plugins/jacks-Q.js
@@ -1,13 +1,15 @@
-var jacksQ =function(request) {
-	var oldSend = request.send;
-	request.send = function() {
-		var defer = Q.defer();
-		oldSend(function(res) {
-			defer.resolve(res);
-		},
-		function(e) {
-			Q.reject(e);
-		})
-		return defer.promise;
-	}
-};
+(function(jacks) {
+	jacks.plugin("Q", function(request) {
+		var oldSend = request.send;
+		request.send = function() {
+			var defer = Q.defer();
+			oldSend(function(res) {
+				defer.resolve(res);
+			},
+			function(e) {
+				Q.reject(e);
+			})
+			return defer.promise;
+		}
+	});
+})(jacks);
--- plugins/jacks-cache.js
+++ plugins/jacks-cache.js
@@ -1,16 +1,19 @@
-jacks.plugin(function jacksCache(request) {
-	jacksCache.cache = jacksCache.cache || {};
-	var oldSend = request.send;
-	request.send = function(callback, error) {
-		// Seuls les gets sont en cache
-		if (requestType === "GET") {
-			if (jacksCache.cache[url]) {
-				callback(jacksCache.cache[url])
-			} else {
-				oldSend(function(response) {
-					callback(response);
-					jacksCache.cache[url] = response;
-				}, error);
+(function(jacks) {
+	jacks.plugin("cache", function jacksCache(request) {
+		jacksCache.cache = jacksCache.cache || {};
+		var oldSend = request.send;
+		request.send = function(callback, error) {
+			// Seuls les gets sont en cache
+			if (request.requestType === "GET") {
+				if (jacksCache.cache[request.url]) {
+					callback(jacksCache.cache[request.url])
+				} else {
+					oldSend(function(response) {
+						jacksCache.cache[request.url] = response;
+						callback(response);
+					}, error);
+				}
 			}
-	}
-})
+		}
+	});
+})(jacks);
