5e7b75112e933720dec0fcb43248fa77898828ef
Jean-Christophe Cazeaux <jccazeaux1979@gmail.com>
2015-05-10 01:27:15 +0200

    Added tests for progress events

--- specs/jacksEventsSpec.js
+++ specs/jacksEventsSpec.js
@@ -25,14 +25,38 @@ describe("Events", function() {
 			});
 	});
 	it("progress", function(done) {
+		var eventUploadCheck = false;
 		var eventCheck = false;
 		jacks()
 			.get(url)
 			.on('progress', function(e) {
 				eventCheck = true;
 			})
+			.on('upload-progress', function(e) {
+				eventUploadCheck = true;
+			})
 			.send(function(response) {
 				expect(eventCheck).toBe(true);
+				expect(eventUploadCheck).toBe(false);
+				done();
+			});
+	});
+	it("upload-progress", function(done) {
+		var eventUploadCheck = false;
+		var eventCheck = false;
+		jacks()
+			.post(url)
+			.body({data:"my data"})
+			.header("Content-Type", "application/json")
+			.on('progress', function(e) {
+				eventCheck = true;
+			})
+			.on('upload-progress', function(e) {
+				eventUploadCheck = true;
+			})
+			.send(function(response) {
+				expect(eventCheck).toBe(true);
+				expect(eventUploadCheck).toBe(true);
 				done();
 			});
 	});
